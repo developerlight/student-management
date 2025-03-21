7e8fffe7e91f668cb88bc966857ea455a3ff6f85
developerlight <samsulmuhyidin@gmail.com>
2025-03-21 23:04:02 +0700

    create upload cloudinary

--- app/components/cloudinaryUploader.jsx
+++ app/components/cloudinaryUploader.jsx
@@ -1,26 +1,64 @@
 'use client';
 
+import { useState } from "react";
 import { cloudinaryEnv } from "../env";
-
-
-const { CldUploadButton } = require("next-cloudinary");
+const { CldUploadButton, CldUploadWidget } = require("next-cloudinary");
 
 const CloudinaryUploader = () => {
-    return ( 
+    const [resource, setResource] = useState(undefined);
+    const handleUpload = async (req, { widget }) => {
+        console.log('upload result:', req);
+
+        console.log('image url:', req.info.secure_url);
+        console.log('publicId:', req.info.public_id);
+        try {
+            const response = req;
+            console.log(response);
+        } catch (error) {
+            console.error("Cloudinary Signature Error:", error);
+        }
+    }
+
+    return (
         <div className="">
-            <CldUploadButton
+            {/* <CldUploadButton
                 options={{
                     multiple: false,
                     sources: ['local', 'url', 'camera', 'google_photos'],
                 }}
                 uploadPreset={cloudinaryEnv.PRESET_NAME}
+                onUploadComplete={handleUpload}
                 className="bg-green-400 py-2 px-3 rounded border mt-4 text-white
                             hover:bg-green-500 transition ease-in-out delay-200"
             >
                 <span>Upload Image</span>
-            </CldUploadButton>
+            </CldUploadButton> */}
+
+            <CldUploadWidget
+                options={{
+                    multiple: false,
+                    sources: ['local', 'url', 'camera', 'google_photos'],
+                }}
+                uploadPreset={cloudinaryEnv.PRESET_NAME}
+                onSuccess={handleUpload}
+                onQueuesEnd={(result, { widget }) => {
+                    widget.close();
+                }}
+            >
+                {({ open }) => {
+                    function handleOnClick() {
+                        setResource(undefined);
+                        open();
+                    }
+                    return (
+                        <button onClick={handleOnClick}>
+                            Upload an Image
+                        </button>
+                    );
+                }}
+            </CldUploadWidget>
         </div>
-     );
+    );
 }
- 
+
 export default CloudinaryUploader;
